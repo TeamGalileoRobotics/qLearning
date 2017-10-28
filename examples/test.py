610b88939e940a216e52d905514975387b75688f
GalileoRobotics <info@galileorobotics.ch>
2017-10-28 11:30:18 +0200

    Make progress

---
+++ examples/test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import time
+import nxt.locator
+from nxt.sensor import *
+from nxt.motor import *
+
+b = nxt.locator.find_one_brick(name = 'NXT')
+
+#Ultrasonic sensor latency test
+m = Motor(b, PORT_A)
+
+m.reset_position(False)
+
+print m.get_tacho().tacho_count
+print m.get_tacho().rotation_count
+
+m.turn(100, 20)
+print m.get_tacho().tacho_count
+print m.get_tacho().rotation_count
+
+m.turn(100, 0)
+print m.get_tacho().tacho_count
+print m.get_tacho().rotation_count
--- nxt/environment.py
+++ nxt/environment.py
@@ -12,11 +12,9 @@ from nxt.motor import *
 
 class Environment:
     running = True
-    
-    UP_UP = 0
-    UP_DOWN = 1
-    DOWN_UP = 2
-    DOWN_DOWN = 3
+
+    SPEED = 50
+    STEP_SIZE = 10
 
     def __init__(self):
         self.state = [0,0]
@@ -24,11 +22,34 @@ class Environment:
         self.nxt = nxt.locator.find_one_brick(name = 'NXT')
         self.ultrasonic = Ultrasonic(nxt, PORT_1)
 
+        self.motor_top = Motor(nxt, PORT_A)
+        self.motor_bottom = Motor(nxt, PORT_B)
+
     def move(self, action):
         start_distance = ultrasonic.get_sample()
-        print start_distance
-        #TODO: move motor 
-        reward = ultrasonic.get_sample() - start_distance
-        self.state = action
+        print 'start distance: ' + start_distance
+        
+        if action == Action.UP_UP:
+            motor_top.turn(SPEED, STEP_SIZE)
+            motor_bottom.turn(SPEED, STEP_SIZE)
+        else if action == Action.UP_DOWN:
+            motor_top.turn(SPEED, STEP_SIZE)
+            motor_bottom.turn(SPEED, 0 - STEP_SIZE)
+        else if action == Action.DOWN_UP:
+            motor_top.turn(SPEED, 0 - STEP_SIZE)
+            motor_bottom.turn(SPEED, STEP_SIZE)
+        else if action == Action.DOWN_DOWN:
+            motor_top.turn(SPEED, 0 - STEP_SIZE)
+            motor_bottom.turn(SPEED, 0 - STEP_SIZE)
 
+        reward = ultrasonic.get_sample() - start_distance
+        self.state = motor_top.
+
+        print 'reward: ' + reward
         return reward
+
+class Action:
+    UP_UP = 0x1
+    UP_DOWN = 0x2
+    DOWN_UP = 0x3
+    DOWN_DOWN = 0x4
